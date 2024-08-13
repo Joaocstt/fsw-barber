388c86d078da188fa87e978913b28743cff793ac
unknown <jnc201820@gmail.com>
2024-08-13 16:36:16 -0300

    svg updated

--- src/app/_constants/search.ts
+++ src/app/_constants/search.ts
@@ -22,7 +22,7 @@ export const quickSearchOption: QuickSearchOption[] = [
       title: "Sobrancelha"
     },
     {
-      imageUrl: "/Massagem.svg",
+      imageUrl: "/massagem.svg",
       title: "Massagem"
     },
     {
