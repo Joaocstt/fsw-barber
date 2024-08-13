9c6debe64b6a2f7a1dbb7e0b99f4b921c383bf68
unknown <jnc201820@gmail.com>
2024-08-13 14:11:34 -0300

    feat: update welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ import Search from "./_components/search";
 import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./_lib/auth";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 
 export default async function Home() {
@@ -48,8 +50,16 @@ export default async function Home() {
       { /* Header */}
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Shanks</h2>
+        <h2 className="text-xl font-bold">Olá, {session?.user ? session.user.name : "bem vindo!"}</h2>
         <p>Segunda-feira, 05 de agosto</p>
+        <p>
+          <span className="capitalize">
+          {format(new Date(), "EEEE, dd", {locale: ptBR} )}
+
+          </span>
+          <span> de {format(new Date(), "MMMM", {locale: ptBR} )}
+          </span>
+        </p>
 
         {/* BUSCA (SEARCH) */}
         <div className="mt-6">
